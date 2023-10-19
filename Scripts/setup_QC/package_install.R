31c208be039d70680f29ddeda0598d9a415bdaa0
Juliana Shin <julianas@n6426.hoffman2.idre.ucla.edu>
2023-10-19 11:49:59 -0700

    add Scripts directory

---
+++ Scripts/setup_QC/package_install.R
@@ -0,0 +1,11 @@
+install.packages("UpSetR")
+install.packages("ggridges")
+install.packages("ComplexUpset")
+install.packages("ggplotify")
+
+BiocManager::install("simplifyEnrichment")
+BiocManager::install("EnhancedVolcano")
+BiocManager::install("biomaRt")
+
+devtools::install_github("rpolicastro/scProportionTest")
+devtools::install_github("jokergoo/ComplexHeatmap")
